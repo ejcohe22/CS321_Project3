9359989808adb280cc1cfb15787f3f9cc299c5be
AidanSweeny <apswee22@colby.edu>
2021-11-01 21:55:05 -0400

    Added add() method for notes page

--- src/app.py
+++ src/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "hello, World!"
+    user = {'username' : 'Aidan' }
+    return render_template("base.html", title="home", user=user)
 
+notes = []
+
+@app.route("/add", methods=["POST"])
+def add():
+    note = request.form.get("Note")
+    notes.append(note)
+    print(notes[-1])
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run()
