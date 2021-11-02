6ebcb57179d32b2c4ee5b36eb558a52157728a1a
AidanSweeny <apswee22@colby.edu>
2021-11-02 13:44:47 -0400

    Added working delete button

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     user = {'username' : 'Aidan' }
-    return render_template("base.html", title="home", user=user)
+    return render_template("base.html", title="home", user=user, notes = notes)
 
 notes = []
 
@@ -13,7 +13,15 @@ notes = []
 def add():
     note = request.form.get("Note")
     notes.append(note)
+    
     print(notes[-1])
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:idx>/", methods=["POST"])
+
+def delete(idx):
+    print("Removing" + str(idx))
+    notes.pop(idx)
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
