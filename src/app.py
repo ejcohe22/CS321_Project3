8f45e55fbc9dd2cbc5148cf37c2a179239fae06b
AidanSweeny <apswee22@colby.edu>
2021-11-03 15:25:42 -0400

    Sorting method and coloring for priority

--- src/app.py
+++ src/app.py
@@ -5,12 +5,11 @@ import datetime
 
 notes = []
 
-
 app = Flask(__name__)
 @app.route("/")
 def index():
     user = {'username' : 'Aidan' }
-    time = datetime.datetime.now() - datetime.timedelta(hours=4)
+    time = datetime.datetime.now()
     time = time.strftime("%B %d, %Y %I:%M %p")
     return render_template("base.html", title="home", user=user, notes = notes, time = time)
 
@@ -20,8 +19,20 @@ def add():
     new_note = request.form.get("Note")
     priority = request.form.get("Priority")
     notes.append(Note(data = new_note, priority = priority, tag = None))
-    
-    print(notes[-1].get_data())
+
+    med_idx = 0
+    low_idx = 0
+    for i in range(len(notes)):
+        if notes[i].get_priority() == "High":
+            notes[0], notes[i]= notes[i], notes[0]
+            med_idx += 1
+            low_idx += 1
+        elif notes[i].get_priority() == "Medium":
+            notes[med_idx], notes[i]= notes[i], notes[med_idx]
+            low_idx += 1
+        else:
+            notes[low_idx], notes[i]= notes[i], notes[low_idx]
+    print(notes)
     return redirect(url_for("index"))
 
 @app.route("/delete/<int:idx>/", methods=["POST"])
