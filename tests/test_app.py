61ec6343b4425d74e9d13dda4b0a24375aea46a3
AidanSweeny <apswee22@colby.edu>
2021-11-09 22:38:56 -0500

    Added tests, but not working

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,6 @@ website_path = "https://cs321-project3-website.herokuapp.com/"
 def test_index():
 	with app.test_client() as test_client:
 		response = test_client.get(website_path)
-		print(website_path + "index")
 		assert response.status_code == 200  # success
 
 
@@ -17,14 +16,14 @@ def test_add():
 	# creating a post request with data as if coming from form
 	with app.test_client() as test_client:
 		url = website_path + 'add'
-		note = Note(data = "new_note", priority = "Medium", tag = "tag")
-		response = test_client.post(url, data=dict(Note=note.data, priority=note.priority, tag=note.tag), follow_redirects=True)
+		note = {"data" : "new_note", "priority" : "Medium", "tag" : "tag"}
 
+		response = test_client.post(url, data=note, follow_redirects=True)
 		# making sure we got redirected, and the request didn't fail
-		assert response.status_code == 302  # redirect
+		assert response.status_code ==  200 # redirect
 		# making sure the home page now includes the added test data
-		# response = test_client.get(url)
-		# assert 'new_note' == response.get_data()
+		response = test_client.get(website_path)
+		assert b'new_note' in response.data
 
 
 
