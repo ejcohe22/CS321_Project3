9c2501cc356c302529dece536d12ffb859e56a75
Erik Cohen <ejcohe22@colby.edu>
2021-11-09 19:11:01 -0500

    wheres source?

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 # Helper File
 import datetime
-from note import Note
+from src.note import Note
 import os
 
 
